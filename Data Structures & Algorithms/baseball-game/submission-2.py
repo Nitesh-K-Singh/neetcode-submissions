982aeb269dd1926c9e98268ee1732e3368729c91
Nitesh Singh <nitesh.singh1601@gmail.com>
2026-04-28 15:02:51 +0530

    Add: baseball-game - submission-2

---
+++ Data Structures & Algorithms/baseball-game/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+
+        result = []
+        ops = operations
+        
+        for i in range(len(ops)):
+            if ops[i].lstrip('-').isdigit():
+                result.append(int(ops[i]))
+            elif ops[i] == "+":
+                result.append(result[-1] + result[-2])  # edge case out of index: no need problem decsription takes care of  it
+            elif ops[i] == "D":
+                result.append(2*result[-1])
+            elif ops[i] == "C":
+                result.pop(-1)
+       
+            
+        
+        
+
+        return sum([int(x) for x in result])
+                
+        
