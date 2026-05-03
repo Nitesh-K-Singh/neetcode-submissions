36f744be338b4b2ca835c10a11f74b864ffaec8f
Nitesh Singh <nitesh.singh1601@gmail.com>
2026-05-03 11:32:21 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+
+        if len(s) - 2*int(len(s)/2) == 1:
+            return False
+        
+        # traverse from left to right, keep adding opening and closing 
+        
+        # dict mapping
+        mapping = {')': '(', '}': '{', ']': '['}
+        mapping_keys = list(mapping.keys())
+        track = []
+        for bracket in s:
+            if bracket in mapping_keys:
+                if len(track)>0 and track[-1] == mapping.get(bracket):
+                    track.pop()
+                else:
+                    return False
+            
+            else:
+                track.append(bracket)
+        
+        # return mapping_keys
+        # return track
+
+        return len(track) == 0
+
+
+     
+
+        
